36f104417264157072c5d250683e092379a6d629
tommy-couzens <tom.czns@gmail.com>
2019-10-10 11:08:14 +0100

    add shpotify

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,18 +1,20 @@
-## install brew
+## Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ## Install command line tools
 brew install hub
 brew install wget
+brew install shpotify
+
 
 # Set git as an alias for hub
 alias git='hub'
 
 
-## Command that puts .bash_profile in
+## Sym link the bash profile defined in this repo to yours
 if [ ! -f  ~/.bash_profile ]; then
     # cp bash_profile.sh ~/.bash_profile
-    echo "hello"
+    ln -s $PWD/bash_profile.sh ~/.bash_profile
 else
    echo "A bash profile already exists. Check if you want to copy anything from this bash_profile.sh"
 fi
